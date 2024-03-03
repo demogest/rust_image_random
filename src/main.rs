d7801581935d12c076613542ba16a0b39094e957
Demogest <demogest@outlook.com>
2024-03-03 21:41:15 +0800

    add upload api

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_multipart::Multipart;
+use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use futures::{StreamExt, TryStreamExt};
+use image::{io::Reader as ImageReader, ImageFormat};
+use md5::{Digest, Md5};
 use rand::Rng;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
@@ -6,6 +10,7 @@ use std::fs;
 use std::fs::File;
 use std::io::{BufReader, Read, Write};
 use std::path::Path;
+use base64::prelude::*;
 use walkdir::WalkDir;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -13,6 +18,7 @@ struct Config {
     host: String,
     port: u16,
     image_folder: String,
+    pwd: String,
 }
 
 fn read_config(config_file: &str) -> Config {
@@ -22,11 +28,12 @@ fn read_config(config_file: &str) -> Config {
             serde_json::from_reader(reader).unwrap()
         }
         Err(_) => {
-            // 当找不到文件时，创建一个默认配置
+            // Dump the default config to the file
             let default_config = Config {
                 host: "127.0.0.1".to_string(),
                 port: 8080,
                 image_folder: "./images".to_string(),
+                pwd: "secret".to_string(),
             };
             let serialized = serde_json::to_string_pretty(&default_config).unwrap();
             let mut file = File::create(config_file).expect("Unable to create config file");
@@ -89,7 +96,9 @@ fn index_images(folder: &str) -> Vec<String> {
         panic!("Image folder does not exist: {}", folder);
     }
     // Validate the structure of the folder, the folder should contain subfolders 'pc' and 'mp'
-    if !Path::new(&format!("{}/pc", folder)).exists() || !Path::new(&format!("{}/mp", folder)).exists() {
+    if !Path::new(&format!("{}/pc", folder)).exists()
+        || !Path::new(&format!("{}/mp", folder)).exists()
+    {
         panic!("Image folder structure is invalid. It should contain subfolders 'pc' and 'mp'.");
     }
 
@@ -108,25 +117,103 @@ fn index_images(folder: &str) -> Vec<String> {
         .collect()
 }
 
+#[actix_web::post("/api/images/{subfolder}")]
+async fn upload_image(
+    mut payload: Multipart,
+    subfolder: web::Path<String>,
+    data: web::Data<Vec<String>>,
+    req: HttpRequest,
+) -> Result<HttpResponse, Error> {
+    let token = data[data.len() - 1].clone();
+    // Record the ip address of the visitor
+    let ip_str = if let Some(cf_ip) = req.headers().get("CF-Connecting-IP") {
+        cf_ip.to_str().unwrap_or("").to_string() // Convert to String
+    } else if let Some(peer_addr) = req.peer_addr() {
+        peer_addr.ip().to_string()
+    } else {
+        "".to_string() // Could not get the ip address
+    };
+    let country = if let Some(cf_country) = req.headers().get("CF-IPCountry") {
+        cf_country.to_str().unwrap_or("Unknown country").to_string()
+    } else {
+        "Unknown country".to_string()
+    };
+    // Check authentication, should be Bearer <token>
+    let auth_header = req.headers().get("Authorization");
+    if auth_header != Some(&actix_web::http::header::HeaderValue::from_str(&format!("Bearer {}", token)).unwrap()) {
+        println!("Unauthorized access from IP: {}, Country: {}", ip_str, country);
+        return Err(actix_web::error::ErrorUnauthorized("Unauthorized."));
+    }
+    // Get the folder path from the data
+    let image_folder = data[data.len() - 2].clone();
+    while let Ok(Some(mut field)) = payload.try_next().await {
+        let content_disposition = field.content_disposition();
+        let filename = match content_disposition.get_filename() {
+            Some(name) => name,
+            None => return Err(actix_web::error::ErrorBadRequest("No filename found.")),
+        };
+        // Get the subfolder from the path as a string
+        let subfolder = subfolder.clone();
+        let folder_path = format!("{}/{}", image_folder, subfolder);
+
+        fs::create_dir_all(&folder_path)?;
+
+        let mut hasher = Md5::new();
+        hasher.update(filename.as_bytes());
+        let hash_result = hasher.finalize();
+        let new_filename = format!("{:x}.webp", hash_result);
+        let new_filepath = format!("{}/{}", folder_path, new_filename);
+
+        let mut buffer = Vec::new();
+        // Read the data from the field
+        while let Some(chunk) = field.next().await {
+            let data = chunk?;
+            buffer.extend_from_slice(&data);
+        }
+
+        // Load the image from the buffer
+        let img = ImageReader::new(std::io::Cursor::new(buffer))
+            .with_guessed_format()
+            .expect("Failed to guess image format")
+            .decode()
+            .expect("Failed to decode image");
+
+        // Save the image to the file
+        img.save_with_format(new_filepath, ImageFormat::WebP)
+            .expect("Failed to save image");
+
+        println!("Image uploaded from IP: {}, Country: {}", ip_str, country);
+    }
+    Ok(HttpResponse::Ok().json("Images uploaded successfully."))
+}
+
 #[actix_web::get("/api/images/{subfolder}")]
-async fn list_images(subfolder: web::Path<String>, data: web::Data<Vec<String>>, req: HttpRequest) -> impl Responder {
+async fn list_images(
+    subfolder: web::Path<String>,
+    data: web::Data<Vec<String>>,
+    req: HttpRequest,
+) -> impl Responder {
+    let data = data[0..data.len() - 2].to_vec();
     // Get the visitor's ip address and print to log
     let ip_str = if let Some(cf_ip) = req.headers().get("CF-Connecting-IP") {
-        cf_ip.to_str().unwrap_or("").to_string() // 转换为String
+        cf_ip.to_str().unwrap_or("").to_string() // Convert to String
     } else if let Some(peer_addr) = req.peer_addr() {
-        peer_addr.ip().to_string() // 已经是String，不需要再次转换
+        peer_addr.ip().to_string()
     } else {
-        "".to_string() // 转换为空String
+        "".to_string() // Could not get the ip address
     };
-    
-    // 由于ip_str现在是String类型，我们在传递给get_country_by_ip时需要传递引用
+
+    // Get the visitor's country and print to log
     let country = if let Some(cf_country) = req.headers().get("CF-IPCountry") {
         cf_country.to_str().unwrap_or("Unknown country").to_string()
     } else {
         "Unknown country".to_string()
     };
 
-    println!("Visitor IP: {}, Country: {}, Subfolder: {}", ip_str, country, subfolder);
+    println!(
+        "Visitor IP: {}, Country: {}, Subfolder: {}",
+        ip_str, country, subfolder
+    );
 
     let subfolder = subfolder.into_inner();
     let filtered_images: Vec<&String> = if subfolder == "all" {
@@ -170,12 +257,19 @@ async fn main() -> std::io::Result<()> {
     // Print the number of images indexed
     println!("Indexed {} images.", images.len());
 
+    let image_folder = config.image_folder.clone();
+    let mut data_vec = images.clone();
+    let token = BASE64_STANDARD.encode(config.pwd.as_bytes());
+    data_vec.push(image_folder);
+    data_vec.push(token);
+
     // Attempt to bind the server to the provided address
     let server = HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(images.clone()))
+            .app_data(web::Data::new(data_vec.clone()))
             .route("/", web::get().to(|| async { "Hello, world!" }))
             .service(list_images)
+            .service(upload_image)
     })
     .bind(format!("{}:{}", config.host, config.port));
 
